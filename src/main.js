627e4a56e7f1e6df29f47a231cefa31ecc5182a7
Cameron <cbutler2018@gmail.com>
2017-10-26 20:19:49 -0400

    Introduce Vuex and Store.

--- src/main.js
+++ src/main.js
@@ -3,16 +3,34 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import Vuetify from 'vuetify';
+import Vuex from 'vuex';
 import App from './App';
 import router from './router';
 
 // index.js or main.js
 import('../node_modules/vuetify/dist/vuetify.min.css'); // Ensure you are using css-loader
 
+Vue.use(Vuex);
 Vue.use(VueResource);
 Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vuex.Store({
+  state: {
+    auth: {
+      token: null,
+    },
+  },
+  mutations: {
+    setToken(state, token) {
+      /* eslint-disable no-param-reassign */
+      state.auth.token = token;
+    },
+  },
+});
+
 
 /* eslint-disable no-new */
 new Vue({
